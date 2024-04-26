2e6edb82378412e951951af4f98b92d942985b78
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-04-26 15:08:32 +0700

    menampilkan pesan link list

--- 077_singlelinkedlist/077_singlelinkedlist.cpp
+++ 077_singlelinkedlist/077_singlelinkedlist.cpp
@@ -89,3 +89,13 @@ bool listEmpty() {
         return false;
 }
 
+
+void traverse() {
+    if (listEmpty()) {
+        cout << "List Kosong" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
+}
+
